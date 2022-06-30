5d925468fb985f61792fde7517930315f357c6b7
Wondosen Seminew <wseminew50@gmail.com>
2022-06-30 15:11:38 -0700

    my betty correction trial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,19 +3,17 @@
 
 
 /**
+ *    * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
+ *    *
  *
  *    * Return: Always 0 (Success)
- *
- *     */
+ *    */
 
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+ puts("\"Programming is like building a multilingual puzzle\n");
 
 	    return (0);
 
