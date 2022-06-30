f49684082c853ebef8d7680b729abdd23ffd0ce9
Wondosen Seminew <wseminew50@gmail.com>
2022-06-30 12:42:43 -0700

    This is a commit on project 0x00.c hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (void)
+{
+**
+
+	 /** This program prints exactly this statementP:
+	  * 
+	  * "Programming is like building
+	  * a multilingual puzzle
+	  *
+          */
+puts(" \"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (void)
+{
+        /**
+	 * This program displays exactly the following text
+	 *
+	 * with proper grammar, but
+        * the outcome is a piece of art,
+         *
+	 */
+printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
