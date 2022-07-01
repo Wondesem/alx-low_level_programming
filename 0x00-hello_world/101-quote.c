5f66e08f66d2d7c23c7d9d8a370cf1374081a475
Wondosen Seminew <wseminew50@gmail.com>
2022-06-30 18:42:38 -0700

    git tired

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 /**
- *main - Entry point
+ *main -entry point
  *
- *Return: Always 1
+ *return: 1
  */
 int main(void)
-{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{	int s;
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 	return (1); }
