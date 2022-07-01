c64a4810427307c1cac48f695374781debddc5c8
Wondosen Seminew <wseminew50@gmail.com>
2022-06-30 19:28:25 -0700

    very challenging

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  *  main - Prints out the last part of a quote in the standard error.
@@ -8,7 +9,7 @@
  */
 
 int main(void)
-{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{	int s;
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 	return (1); }
-
-
