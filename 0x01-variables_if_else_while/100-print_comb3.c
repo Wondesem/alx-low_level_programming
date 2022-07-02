c9b96e8c09488b0fe3c40a3499267cce6ac2bb54
Wondosen Seminew <wseminew50@gmail.com>
2022-07-02 12:54:20 -0700

    revision

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,28 +6,19 @@
  * Return: always 0 (success)
  */
 int main(void)
-{int x, y, q;
-for (x = 48; x < 58; x++)
-{
-for (y = 49; y < 58; y++)
-{
-	for (q = 50; q < 58; q++)
+{int x, y;
+	for (x = 0; x < 9; x++)
 	{
-		if (q > y && y > x)
+		for (y = x + 1; y < 10; y++)
 		{
-			putchar(x);
-			putchar(y);
-			putchar(q);
-			if (x != 55 || y != 56)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
-}
-}
-putchar('\n');
-return (0);
-
+	putchar('\n');
+	return (0);
 }
