d3dad0e48311763fb09737b8ee5666aa2b777898
Wondosen Seminew <wseminew50@gmail.com>
2022-07-05 13:54:17 -0700

    hands on practices

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
- * main - prints _putchar
+ * main - prints putchar
  * followed by new line
  *
  * Return: always 0
  */
+
 int main(void)
 {
 _putchar('_');
@@ -16,4 +18,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0); }
+
+return (0);
+}
