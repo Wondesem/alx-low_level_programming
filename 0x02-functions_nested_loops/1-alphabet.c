bb5b190d3424e50af1d1873674e5ebb8af6263fa
Wondosen Seminew <wseminew50@gmail.com>
2022-07-05 05:57:42 -0700

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints lower case alpabets
+ * followed by new line
+ *
+ * Return: always return 0
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+return (0); }
+/**
+ * int main(void)
+{
+print_alphabet()
+return (0); }*/
