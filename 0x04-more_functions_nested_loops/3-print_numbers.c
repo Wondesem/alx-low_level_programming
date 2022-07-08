cfb40cda04dfbedd3464362ac91d3156bb46f96e
Wondosen Seminew <wseminew50@gmail.com>
2022-07-08 03:42:02 -0700

    task on more nested loop functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers -prints numbers from 0 to 9
- * followed by new line
- * Return: always 0
+ * * print_numbers - Prints the numbers from 0-9.
  */
 void print_numbers(void)
 {
-int  a;
+	int a;
 
-a = 0;
-while (a < 10)
-{
-_putchar((a % 10) + '0');
-a++;
+	for (a = 0 ; a <= 9; a++)
+		_putchar((a % 10) + '0');
+	_putchar('\n');
 }
 
-_putchar('\');
-}
+
