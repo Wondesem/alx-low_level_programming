cea13fc5a4c113c6fd5b23d4d6ff4a4215fb7c44
Wondosen Seminew <wseminew50@gmail.com>
2022-07-11 17:28:13 -0700

    task on pointer, array and string functions

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {int len, i;
-i = 0;
+len = 0;
 while (src[len] != '\0')
+{len++;
+}
+for (i = 0; i < len; i++)
 {dest[i] = src[i];
 }
 dest[i] = '\0';
 return (dest);
 }
+
