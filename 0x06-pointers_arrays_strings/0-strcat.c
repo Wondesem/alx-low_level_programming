481f397b67068ebaa1d887208e54656e35b19cb6
Wondosen Seminew <wseminew50@gmail.com>
2022-07-14 03:47:43 -0700

    commit on 0x06-pointers_arra

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings.
- * @src: first string to be concatenate
- * @dest: second string to append
+ * _strcat - concatenates two strings.
+ * @src: first string to be concatenated
+ * @dest: second string to be appended
  * Return: dest
  */
 char *_strcat(char *dest, char *src);
 {int i, j;
-for (i = 0; dest[i] != '\0'; i++)
-for (j = 0; src[j] != '\0'; j++)
-{dest[i] = str[j];
+while (dest[i] )
 	i++;
-}
+for (j = 0; src[j] != '\0'; j++)
+dest[i] = str[j];
+
 return (dest);
 }
