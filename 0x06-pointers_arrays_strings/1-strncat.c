f2f503056ab8a974c4f37e706cdcf4e838435402
Wondosen Seminew <wseminew50@gmail.com>
2022-07-15 11:56:09 -0700

    commit on task 0x06

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,13 +7,13 @@
  *
  * Return: returns pointer to dest sting result
  */
+
 char *_strncat(char *dest, char *src, int n)
 {int i, j;
-	i = 0;
-	while (dest[i] != '\0')
-		i++;
-	for (j = 0; src[j] != '\0' && n > 0; j+, n--, i++)
+	while (dest[i] != '\0'; i++)
+		j++;
+	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
 	{dest[i] = src[j];
 	}
-return (dest);
+	return (dest);
 }
