0e1171c0ee135275c5eddbee235f4cef69b071b9
Wondosen Seminew <wseminew50@gmail.com>
2022-07-15 13:17:52 -0700

    commit on task 0x06-poiners_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,10 +9,10 @@
  */
 
 char *_strncpy(char *dest, char *src, int n)
-{int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[i] = src[i];
-for (; i > n; i++)
-dest[i] = '\0';
+{int x;
+for (x = 0; x < n && src[x] != '\0'; x++)
+	dest[x] = src[x];
+for (; n > x; x++)
+	dest[x] = '\0';
 return (dest);
 }
