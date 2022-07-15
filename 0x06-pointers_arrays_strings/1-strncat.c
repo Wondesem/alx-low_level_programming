f4d146055ddd85299e32645893c7467917fbc87f
Wondosen Seminew <wseminew50@gmail.com>
2022-07-15 11:59:39 -0700

    commit on task 0x06

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,7 @@
 char *_strncat(char *dest, char *src, int n)
 {int i, j;
 	while (dest[i] != '\0'; i++)
-		j++;
+		i++;
 	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
 	{dest[i] = src[j];
 	}
