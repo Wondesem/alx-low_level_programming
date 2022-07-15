f3d4330ca65cffb75fc2e379d25a51ba1ae58d3e
Wondosen Seminew <wseminew50@gmail.com>
2022-07-15 10:36:34 -0700

    commit on task 0x06

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,8 +7,14 @@
  *
  * Return: returns pointer to @dest
  */
-char *_strcat(char *dest, char *src);
-{	while (*dest) ++dest;
-	while (*dest++ = *src++);
-	return (dest);
+char *_strcat(char *dest, char *src)
+{int x, y;
+for (x = 0; dest[x] != '\0'; x++)
+	;
+for (y = 0; src[y] != '\0'; y++)
+{dest[x] = src[y];
+	x++;
+}
+dest[x] = '\0';
+return (dest);
 }
