f5ee2b8e96b5f71e8f78284ddd94d9d6e03b31e7
Wondosen Seminew <wseminew50@gmail.com>
2022-07-18 13:13:06 -0700

    commit on task 0x07 pointers arrays and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+
+#include "main.h"
+/**
+ * _strspn - gets length of prefix subscripts
+ * @s: initial byte segment
+ * @accept: accepted string
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{unsigned int a = 0, b, t = 0;
+	while (accept[a])
+	{
+		b = 0;
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{t++; 
+			}
+			b++;
+		}
+		a++;
+	}
+return (t);
+}
