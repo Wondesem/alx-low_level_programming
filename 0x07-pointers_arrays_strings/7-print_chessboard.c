ae65d20cdd73d6ec163ebb03adfdd71045af0ba4
Wondosen Seminew <wseminew50@gmail.com>
2022-07-19 09:25:47 -0700

    commit on task 0x07 pointers arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,15 +6,12 @@
  * Return: returns nothing
  */
 void print_chessboard(char (*a)[8])
-{int i = 0, j = 0;
-	while (i < 8)
+{int i, j;
+	for (i = 0; i < 8; i++)
 	{
-		while (j < 8)
-		{_putchar(a[i][j]);
-			j++
-		}
-		_putchar('\n');
-		i++;
-	}
+		for (j = 0; j < 8; j++)
+		{_putchar(a[i][j]; }
+				_putchar('\n');
+				}
 }
 
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,17 +1,17 @@
 
 #include "main.h"
 /**
- * print_diagsum - Print the sum of two diagonals of square matrix
+ * print_diagsums - Print the sum of two diagonals of square matrix
  * @a: pointer to arrays of diagonals
  * @size: the matrix size
  * Return: nothimg
  */
 void print_diagsums(int *a, int size)
-{int i = 0, sum1 = 0, sum2 = 0;
-	while (i < size)
+{int i, sum1 = 0, sum2 = 0;
+	for (i = 0; i < size; i++)
 	{sum1 += a[(size + 1) * i];
 	sum2 += a[(size - 1) * (i + 1)];
-	printf("Sum1 = %d. Sum2 = %d", sum1, sum2);
 	}
+	printf("Sum1 = %d. Sum2 = %d", sum1, sum2);
 }
 
