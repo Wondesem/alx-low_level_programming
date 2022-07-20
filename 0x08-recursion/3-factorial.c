251f48f94fa2f4e98e0b867215accbc9114341be
Wondosen Seminew <wseminew50@gmail.com>
2022-07-20 11:08:59 -0700

    recursion is fantastic

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -13,8 +13,9 @@ int factorial(int n)
 
 	if (n < 0)
 		return (-1);
+
 	else if (n >= 0 && n <= 1)
 		return (1);
-		fact = fact * factorial(n - 1);
+	fact = fact * factorial(n - 1);
 		return (fact);
 }
