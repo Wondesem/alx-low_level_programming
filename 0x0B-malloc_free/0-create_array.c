d10884183ed2524e5dee9c5c3f18f198c9bc9934
Wondosen Seminew <wseminew50@gmail.com>
2022-07-26 13:00:47 -0700

    task 0x0b malloc and free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_array - creates an array of chars
+ * @size: length of array
+ * @c: character array created
+ *
+ * Return: pointer to the array
+ *         or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int x;
+		char *m;
+
+		if (size == 0)
+			return (NULL);
+		m = malloc(size * sizeof(char));
+
+		if (m == 0)
+		{
+			return (NULL);
+		}
+		for (x = 0; x < size; x++)
+		{
+			m[x] = c;
+		}
+		return (m);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strdup - duplicate string
+ * @str: string to be copied
+ * Return: return pointer to duplicated string
+ *         NULL if str is NULL
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+
+	char *sr
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+	for (i = 0; str[i] != '\0'; i++)
+		j++;
+	sr = malloc((j + 1) * sizeof(char));
+	if (sr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		sr[i] = str[i];
+	}
+	sr[j] = '\0';
+	return (sr);
+
+
+}
