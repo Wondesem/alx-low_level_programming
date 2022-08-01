5ec05952e8b9a5bbafccfbf150a2da223ef706eb
Wondosen Seminew <wseminew50@gmail.com>
2022-08-01 13:05:46 -0700

    Task 0x0E structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,7 +20,7 @@ return (NULL);
 for (nl = 0; name[nl]; nl++)
 ;
 nl++;
-dog->name = malloc(nl * sizeof(char));
+dog->name = malloc(sizeof(char) * nl);
 if (dog->name == NULL)
 {
 free(dog);
@@ -32,7 +32,7 @@ dog->age = age;
 for (ol = 0; owner[ol]; ol++)
 ;
 ol++;
-dog->owner = malloc(ol * sizeof(char));
+dog->owner = malloc(sizeof(char) * ol);
 if (dog->owner == NULL)
 {
 free(dog->name);
