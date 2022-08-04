623f87d7956d4ab863c80f1d1e11ee6bc428ae1f
Wondosen Seminew <wseminew50@gmail.com>
2022-08-04 12:31:27 -0700

    initial commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,11 +7,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-va_list nums;
-unsigned int index, sum = 0;
-va_start(nums, n);
-for (index = 0; index < n; index++)
-sum += va_arg(nums, int);
-va_end(nums);
+int sum = 0;
+unsigned int i;
+va_list valist;
+va_start(valist, n);
+for (i = 0; i < n; i++)
+sum += va_arg(valist, int);
+va_end(valist);
 return (sum);
 }
+
