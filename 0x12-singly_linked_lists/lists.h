e7c30d6c0106ad05f1d8f5e529c1bc72a12be70c
Wondosen Seminew <wseminew50@gmail.com>
2022-08-11 08:28:47 -0700

    initial commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,18 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+/**
+ * typedef struct lists_s - lists singly linked lists
+ * @str: character string in the list
+ * @len: lenght of linked list strengs
+ * @next: the next pointer in node link
+ */
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
@@ -7,4 +20,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif /* LIST_H */
+#endif /* LISTS_H */
