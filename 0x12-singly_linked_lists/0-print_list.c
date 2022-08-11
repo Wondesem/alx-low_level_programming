b978c6b279d1ec9230439a3d02d3ae88f8f48e4c
Wondosen Seminew <wseminew50@gmail.com>
2022-08-11 11:58:48 -0700

    initial commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,16 +6,16 @@
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
-{size_t nodes = 0;
+{size_t link = 0;
 
-while (h)
+while (h != NULL)
 {
 if (h->str == NULL)
 printf("[0] (nil)\n");
 else
 printf("[%d] %s\n", h->len, h->str);
-nodes++;
+link++;
 h = h->next;
 }
-return (nodes);
+return (link);
 }
