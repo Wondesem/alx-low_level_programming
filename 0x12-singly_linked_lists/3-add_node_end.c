0eb00c50ff9552b03ea8acac5c4b5bdc0a576048
Wondosen Seminew <wseminew50@gmail.com>
2022-08-11 20:52:49 -0700

    first try to correct task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * _strlen - calculates string length
+ * @str: string to be calclated
+ * Return: number of nodes
+ */
+unsigned int _strlen(char *str)
+{
+unsigned int i;
+for (i = 0; str[i]; i++)
+;
+return (i);
+}
+
+/**
  * add_node_end - adds node to the end of list
  * @head: the first node of the list
  * @str: the character string pointer
@@ -9,31 +22,28 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-char *dup;
-int len;
-list_t *new, *last;
+list_t *new, *tmp;
+if (str == NULL)
+return (NULL);
 new = malloc(sizeof(list_t));
 if (new == NULL)
 return (NULL);
-dup = strdup(str);
-if (str == NULL)
+new->str = strdup(str);
+if (new->str == NULL)
 {
 free(new);
 return (NULL);
 }
-for (len = 0; str[len];)
-len++;
-new->str = dup;
-new->len = len;
-ew->next = NULL;
+new->len = _strlen(new->str);
+new->next = NULL;
 if (*head == NULL)
-*head = new;
-else
 {
-last = *head;
-while (last->next != NULL)
-last = last->next;
-last->next = new;
+*head = new;
+return (new);
 }
-return (*head);
+tmp = *head;
+while (tmp->next)
+tmp = tmp->next;
+tmp->next = new;
+return (new);
 }
