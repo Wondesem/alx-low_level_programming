2e4dafc36f10bc3b898addce665b2f7baf3617e8
Wondosen Seminew <wseminew50@gmail.com>
2022-08-11 21:04:15 -0700

    second try to correct task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,14 +6,16 @@
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
-{size_t i = 0;
-const list_t *ptr;
-ptr = h;
-while (ptr)
 {
-printf("[%d] %s\n", ptr->len, ptr->str);
-ptr = ptr->next;
-i++;
+size_t nodes = 0;
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+nodes++;
+h = h->next;
 }
-return (i);
+return (nodes);
 }
