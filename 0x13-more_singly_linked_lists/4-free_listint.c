1ede77b34157f8619c830dd71e69549a84262830
Wondosen Seminew <wseminew50@gmail.com>
2022-08-15 12:25:35 -0700

    commit on task freeing list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listin - frees a listint_t list
+ * @head - head pointer in the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+if (head == NULL)
+return;
+while (*head)
+{
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+}
+head = NULL;
+}
