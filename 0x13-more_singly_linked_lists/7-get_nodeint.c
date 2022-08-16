4ff8371c337df331b179538fcd6dbe85a4efde6c
Wondosen Seminew <wseminew50@gmail.com>
2022-08-16 11:35:26 -0700

    task 7 get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_node-at_index
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: pointer to the address of head
  * @index: index to locate node starting at 0
  * Return: returns null if not exists otherwise node
