4653b688ecef19aaf927edd472b2b2849f1a59d5
Wondosen Seminew <wseminew50@gmail.com>
2022-08-16 06:51:06 -0700

    commit on task 4 on freeing list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,13 @@
 void free_listint2(listint_t **head)
 {
 listint_t *temp;
-if (*head == NULL)
+if (head == NULL)
 return (NULL);
 while (*head)
 {
 temp = (*head)->next;
 free(*head);
-(*head) = temp;
+*head = temp;
 }
+head = NULL;
 }
