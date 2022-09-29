ba1012d925dfc1c3597c243881a7566501e44709
wondesem <wseminew50@gmail.com>
2022-09-29 05:40:50 -0700

    It is commit on doubl linked list project

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,19 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_dlistint - free a dlistint_t list.
- * @head: pointer to pointer to head
- * Return: void
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @n: The integer for the new node to contain.
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
-void free_dlistint(dlistint_t *head)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *tem;
-while (head != NULL)
+dlistint_t *new, *last;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (*head == NULL)
 {
-tem = head;
-head = head->next;
-free(tem);
+new->prev = NULL;
+*head = new;
+return (new);
 }
+last = *head;
+while (last->next != NULL)
+last = last->next;
+last->next = new;
+new->prev = last;
+return (new);
 }
