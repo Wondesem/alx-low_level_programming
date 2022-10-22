9a833ca076504eee07a1b4a30162dfea88345056
wondesem <wseminew50gmail.com>
2022-10-22 04:27:37 -0700

    advanced task

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,7 +1,7 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
@@ -12,10 +12,11 @@
  */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
+
 /**
  * struct hash_table_s - Hash table data structure
  * @size: The size of the array
@@ -23,11 +24,10 @@ typedef struct hash_node_s
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
-
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
@@ -36,8 +36,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-/*Other functions*/
-unsigned long int hash_djb2(const unsigned char *str);
 /**
  * struct shash_node_s - Node of a sorted hash table
  * @key: The key, string
@@ -49,11 +47,11 @@ unsigned long int hash_djb2(const unsigned char *str);
  */
 typedef struct shash_node_s
 {
-	char *key;
-	char *value;
-	struct shash_node_s *next;
-	struct shash_node_s *sprev;
-	struct shash_node_s *snext;
+char *key;
+char *value;
+struct shash_node_s *next;
+struct shash_node_s *sprev;
+struct shash_node_s *snext;
 } shash_node_t;
 /**
  * struct shash_table_s - Sorted hash table data structure
