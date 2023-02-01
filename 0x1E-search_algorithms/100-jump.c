78cd4ac80156360780ec424808f1895977b6bd2a
wondesem <wseminew50gmail.com>
2023-01-31 22:14:34 -0800

    task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,7 +12,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t i, start = 0, end = sqrt(size);
 
-	if (!array)
+	if (array == NULL || size == 0)
 		return (-1);
 	while (array[end] < value && start < size)
 	{
